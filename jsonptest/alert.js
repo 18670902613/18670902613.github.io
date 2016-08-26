b37513da4307e13a1c6e360efac4dd5caafd6356
18670902613 <ben18670902613@gmail.com>
2016-08-26 17:37:41 +0800

    jsonptest-photo

--- jsonptest/alert.js
+++ jsonptest/alert.js
@@ -1,2 +1,11 @@
-myFn(100);
+
+
+    var data = {
+        "name":"Mark",
+        "age":"36",
+        "sex":"男",
+        "photo":"https://18670902613.github.io/jsonptest/images/pptImg.jpg"
+    }
+
+    myFn(data);
 
